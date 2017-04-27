67aae923a637745aaf944b3c8c766f6dbeee44b7
chenyvehtung <chenyvehtung@163.com>
2017-04-27 13:58:26 +0800

    fix bug

--- params_config.py
+++ params_config.py
@@ -39,16 +39,16 @@ class MNIST_Config:
 
 class CIFAR_Config:
     # parameters of model
-    learning_rate = 0.05
+    learning_rate = 0.025
     momentum = 0.5
     batch_size = 100
     n_epoch = 40001
-    n_train_discriminator = 1
+    n_train_discriminator = 2
     n_train_generator = 1
 
     # parameters of input & output
     image_size = 32*32*3
-    input_size = 12*12*3
+    input_size = 10*10
 
     # hidden parameters of generator
     g_n_hidden1 = 8000
